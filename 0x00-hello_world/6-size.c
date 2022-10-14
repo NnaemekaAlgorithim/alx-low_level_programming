514f1d2c4cec4e4933cff1e64a2a6daad8c52f5b
NnaemekaAlgorithim <you@example.com>
2022-10-14 07:28:37 -0700

    Scripting C files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,15 +12,17 @@ int main(void)
 
 {
 
-int intType;
-float floatType;
-double doubleType;
 char charType;
+int intType;
+long int longInt;
+long long int longLongInt;
+float floatType;
 
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
-printf("Size of double: %zu bytes\n", sizeof(doubleType));
-printf("Size of char: %zu byte\n", sizeof(charType));
+printf("Size of a char: %zu byte\n", sizeof(charType));
+printf("Size of an int: %zu byte\n", sizeof(intType));
+printf("Size of a long int: %zu byte\n", sizeof(longInt));
+printf("Size of a long long int: %zu byte\n", sizeof(longLongInt));
+printf("Size of a float: %zu byte\n", sizeof(floatType));
 
 return (0);
 
