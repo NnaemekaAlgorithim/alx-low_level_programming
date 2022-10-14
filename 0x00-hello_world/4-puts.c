88ff81167b122dbf48ff9d70c7bb2ee9b09a46a8
NnaemekaAlgorithim <you@example.com>
2022-10-13 20:10:34 -0700

    Scripting C files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,18 +4,18 @@
 
 /**
 
- * a simple programe to print text.
- *
- * just for my ALX course
- *
- */
+a simple programe to print text.
+
+just for my ALX course
+
+*/
 
 int main(void)
-  
+
 {
-  
+
   puts("\"Programming is like building a multilingual puzzle");
 
   return (0);
-     
+
 }
