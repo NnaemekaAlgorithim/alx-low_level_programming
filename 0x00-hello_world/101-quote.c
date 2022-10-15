002a451ce1d6e5b292f3b9a041d51bd7722197c0
NnaemekaAlgorithim <you@example.com>
2022-10-15 06:59:16 -0700

    Scripting C files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,16 +3,21 @@
 #include <unistd.h>
 
 /**
-* main - Entry point
-* Return: 1 if success.
-*/
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 1 (Success)
+
+ */
 
 int main(void)
-
+  
 {
-
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
-
-return (1);
-
+  
+  write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+  
+  return (1);
+  
 }
