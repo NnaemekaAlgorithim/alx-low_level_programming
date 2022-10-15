2a1762abd60d7b8b885dd4e59900200ebd3808ed
NnaemekaAlgorithim <you@example.com>
2022-10-15 06:02:08 -0700

    Scripting C files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,18 +4,18 @@
 
 /**
 
- * main - Prints out the last part of a quote in the standard error.
+* main - Prints out the last part of a quote in the standard error.
 
- * Return: 1 if success.
+* Return: 1 if success.
 
- */
+*/
 
 int main(void)
-  
+
 {
-  
-  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
-  
-  return (1);
-  
+
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n\n", 58);
+
+return (1);
+
 }
