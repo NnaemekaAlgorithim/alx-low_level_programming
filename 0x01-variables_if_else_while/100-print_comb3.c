dfa33ac0c7b470eebac9d9c965720d79da9f4daa
NnaemekaAlgorithim <you@example.com>
2022-10-17 04:07:26 -0700

    C programing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,10 +12,10 @@ int main(void)
 int i, j;
 for (i = '0'; i <= '9'; i++)
 {
-for (j = '1'; j < '9'; j++)
+for (j = i; j < '9'; ++j)
 {
 putchar(i);
-putchar(j);
+putchar(j+1);
 putchar(',');
 putchar(' ');
 }
