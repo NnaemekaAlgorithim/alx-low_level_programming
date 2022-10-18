a9b49be645e96455a6aae730aa7b68a41dbd5987
NnaemekaAlgorithim <you@example.com>
2022-10-18 13:47:35 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,20 @@
-#include <unistd.h>
-
+#include "main.h"
 
 /**
 * main - Entry point
 * Return: Always 0 (Success)
 */
 
-
 int main(void)
 {
-printf("_putchar\n");
+char x[9] = {'_','p','u','t','c','h','a','r','\0'};
+int i;
+
+for (i = 0; i <= 8; i++)
+{
+_putchar(x[i]);
+}
+_putchar('\n');
+
 return (0);
 }
