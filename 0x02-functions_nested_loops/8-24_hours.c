b98d9e81156900c9aeab7a2bba1b903661bad2c5
NnaemekaAlgorithim <you@example.com>
2022-10-19 06:16:29 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <unistd.h>
+
+
+/**
+* jack_bauer - Entry point
+* Return: On success 0.
+* On error, -1 is returned, and errno is set appropriately
+*/
+
+void jack_bauer(void)
+  
+{
+int hour, minute;
+
+for (hour = 0; hour <= 23; hour++)
+{
+
+for (minute = 0; minute <= 59; minute++)
+{
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar(':');
+_putchar((minute / 10) + '0');
+_putchar((minute % 10) + '0');
+_putchar('\n');
+}
+
+}
+
+}
