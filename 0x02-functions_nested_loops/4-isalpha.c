c953f90f8adbc4c4b13a620d6a86f699c29c8722
NnaemekaAlgorithim <you@example.com>
2022-10-18 20:20:30 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 
 
 /**
-* _islower - Entry point
+* _isalpha - Entry point
 * @c: The character to take input.
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately
@@ -13,7 +13,7 @@
 int _isalpha(int c)
 {
 
-  if ((c >= 'a' && c <= 'z') && (c >= 'A' && c <= 'Z'))
+if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
 return (1);
 }
