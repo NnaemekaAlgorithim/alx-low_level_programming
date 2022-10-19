23233562c02c1222ca5ff271b6f8f082527effe2
NnaemekaAlgorithim <you@example.com>
2022-10-19 03:28:50 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <unistd.h>
+
+
+/**
+* print_last_digit - Entry point
+* @c: The character to take input.
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately
+*/
+
+
+int print_last_digit(int c)
+{
+
+int last_digit;
+
+last_digit = c % 10;
+
+return (last_digit);
+
+}
