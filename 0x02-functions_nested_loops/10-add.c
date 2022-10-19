0a9be3b4a47824b8ccba7a18545f09a4414e759a
NnaemekaAlgorithim <you@example.com>
2022-10-19 08:23:10 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,6 +4,8 @@
 
 /**
 * add - Entry point
+* @x: The character to take input.
+* @y: The character to take input.
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately
 */
