cda297a70d1287e425dfa7b1d74f2b863072def9
NnaemekaAlgorithim <you@example.com>
2022-10-19 04:07:59 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -16,6 +16,12 @@ int print_last_digit(int c)
 int last_digit;
 
 last_digit = c % 10;
+if (last_digit < 0) {
+putchar('-');
+last_digit = -last_digit;
+}
+if (last_digit / 10 != 0)
+print_last_digit (last_digit / 10);
 _putchar((last_digit % 10) + '0');
 
 return (last_digit);
