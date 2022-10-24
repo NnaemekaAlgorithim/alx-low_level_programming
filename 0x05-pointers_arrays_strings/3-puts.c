d18ef410e7a67e05cae61c0e8a32fb560352b857
NnaemekaAlgorithim <you@example.com>
2022-10-24 05:18:16 -0700

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+* _puts - Entry point
+* @str: The variable to take input.
+* Return: Always 0 (Success)
+*/
+
+
+void _puts(char *str)
+{
+int i;
+for (i = 0; i < *str.lenght; i++)
+{
+_putchar(*str[i]);
+}
+}
