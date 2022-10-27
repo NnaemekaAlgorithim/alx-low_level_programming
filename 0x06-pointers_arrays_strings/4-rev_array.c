4a9920a22c81a9bcf15cda1d217a83210b3c4f8d
NnaemekaAlgorithim <you@example.com>
2022-10-27 07:57:23 -0700

    more pointers strungs and arrays

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,12 +9,14 @@
 
 void reverse_array(int *a, int n)
 {
-int *tmp, count, i;
+int tmp[100], count, i;
 count = 0;
 i = 0;
 
 for (count = (n - 1); count; count--)
-tmp[i++] = a[count];
+{
+tmp[i] = a[count];
+}
 
 for (i = 0; i < (n - 1); i++)
 a[i] = tmp[i];
