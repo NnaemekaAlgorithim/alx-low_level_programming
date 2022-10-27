f264fda13c51d989d9b3ad4a274e0aef34ea957b
NnaemekaAlgorithim <you@example.com>
2022-10-27 03:48:45 -0700

    more pointers strungs and arrays

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,7 +17,7 @@ while (dest[index++])
 dest_len++;
 
 for (index = 0; src[index] && index < n; index++)
-dest[dest++] = src[index];
+dest[dest_len++] = src[index];
 
 return (dest);
 /*The only difference with the first code is the n condition*/
