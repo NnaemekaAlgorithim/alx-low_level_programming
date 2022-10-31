70f8108bd0539b34fb5beaf3d89e09e8370ca323
NnaemekaAlgorithim <you@example.com>
2022-10-31 14:50:11 -0700

    more pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,7 @@
 
 /**
 * memset - Fills the first n bytes of the memory area
-*          pointed to by @s with the constant byte @c.
+*          pointed to by @s with the constant byte @b.
 * @s: A pointer to the memory area to be filled.
 * @b: The character to fill the memory area with.
 * @n: The number of bytes to be filled.
@@ -13,8 +13,8 @@
 
 void *_memset(char *s, char b, unsigned int n)
 {
-unsigned int index;
-unsigned char *memory = s, value = b;
+int index;
+char *memory = s, value = b;
 
 for (index = 0; index < n; index++)
 memory[index] = value;
