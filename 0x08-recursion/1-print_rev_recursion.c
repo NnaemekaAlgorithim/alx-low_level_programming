087eb2ea88c2d81add6828ce8bcfd40c6d366013
NnaemekaAlgorithim <you@example.com>
2022-11-02 09:06:39 -0700

    Recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _puts_rev_recursion - uses recursion to print strings and a new line
+* @s: the string to be printed in reverse.
+* Return: returns (0) on success.
+*/
+
+void _puts_rev_recursion(char *s)
+{
+
+if (*s)
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+
+}
