90753bdd0e7da48427c5eed6d5c9d626c87b7a58
NnaemekaAlgorithim <you@example.com>
2022-11-02 14:03:36 -0700

    Recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _pow_recursion - uses recursion to print strings and a new line
+* @x: the value to raise its power.
+* @y: the power to raise x to
+* Return: returns (power of x) on success.
+*/
+
+int _pow_recursion(int x, int y)
+{
+  if (y < 0)
+    return (-1);
+
+  if (y = 0)
+    return (1);
+
+  if (y = 1)
+    return (x);
+
+  x *= _pow_recursion(x, (y - 1));
+    
+}
