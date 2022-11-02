d247ef352141c7c6b5951e6e51f7283cf1990021
NnaemekaAlgorithim <you@example.com>
2022-11-02 12:47:00 -0700

    Recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strlen_recursion - uses recursion to print strings and a new line
+* @s: the string length to be printed.
+* Return: returns (0) on success.
+*/
+
+int _strlen_recursion(char *s)
+{
+int len = 0;
+
+while (*s)
+{
+len++;
+len += _strlen_recursion(s - 1);
+}
+
+return (len);
+}
