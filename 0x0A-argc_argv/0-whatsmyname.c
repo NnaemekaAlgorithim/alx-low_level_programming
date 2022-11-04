06531d7fca7692f8bdfc49d19c9131b5ee8d93a8
NnaemekaAlgorithim <you@example.com>
2022-11-04 06:04:54 -0700

    argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - main entry point for the program
- * @argc: the variable to hold the number of arguments
- * @argv: vector variable to hold the name of the arguments
- * Return: return success (0)
- */
+* main - main entry point for the program
+* @argc: the variable to hold the number of arguments
+* @argv: vector variable to hold the name of the arguments
+* Return: return success (0)
+*/
 
-int main (int __attribute__((__unused__)) argc, char* argv[])
+int main (int argc, char* argv[])
 {
 printf("%s\n", argv[0]);
 return (0);
